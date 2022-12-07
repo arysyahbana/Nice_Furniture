abb86fcdd1f9dc1e8ed5ad68171ce645c95383f8
Ary Syahbana <arysyahbana1@gmail.com>
2022-12-07 13:54:02 +0700

    finish

---
+++ js/detail.js
@@ -0,0 +1,100 @@
+// Awal Card Slider 2
+// var container = document.getElementById('container2')
+// var slider = document.getElementById('slider');
+// var slides = document.getElementsByClassName('slide').length;
+// var buttons = document.getElementsByClassName('btn');
+
+
+// var currentPosition = 0;
+// var currentMargin = 0;
+// var slidesPerPage = 0;
+// var slidesCount = slides - slidesPerPage;
+// var containerWidth = container.offsetWidth;
+// var prevKeyActive = false;
+// var nextKeyActive = true;
+
+// window.addEventListener("resize", checkWidth);
+
+// function checkWidth() {
+//     containerWidth = container.offsetWidth;
+//     setParams(containerWidth);
+// }
+
+// function setParams(w) {
+//     if (w < 551) {
+//         slidesPerPage = 1;
+//     } else {
+//         if (w < 901) {
+//             slidesPerPage = 2;
+//         } else {
+//             if (w < 1101) {
+//                 slidesPerPage = 3;
+//             } else {
+//                 slidesPerPage = 4;
+//             }
+//         }
+//     }
+//     slidesCount = slides - slidesPerPage;
+//     if (currentPosition > slidesCount) {
+//         currentPosition -= slidesPerPage;
+//     };
+//     currentMargin = - currentPosition * (100 / slidesPerPage);
+//     slider.style.marginLeft = currentMargin + '%';
+//     if (currentPosition > 0) {
+//         buttons[0].classList.remove('inactive');
+//     }
+//     if (currentPosition < slidesCount) {
+//         buttons[1].classList.remove('inactive');
+//     }
+//     if (currentPosition >= slidesCount) {
+//         buttons[1].classList.add('inactive');
+//     }
+// }
+
+// setParams();
+
+// function slideRight() {
+//     if (currentPosition != 0) {
+//         slider.style.marginLeft = currentMargin + (100 / slidesPerPage) + '%';
+//         currentMargin += (100 / slidesPerPage);
+//         currentPosition--;
+//     };
+//     if (currentPosition === 0) {
+//         buttons[0].classList.add('inactive');
+//     }
+//     if (currentPosition < slidesCount) {
+//         buttons[1].classList.remove('inactive');
+//     }
+// };
+
+// function slideLeft() {
+//     if (currentPosition != slidesCount) {
+//         slider.style.marginLeft = currentMargin - (100 / slidesPerPage) + '%';
+//         currentMargin -= (100 / slidesPerPage);
+//         currentPosition++;
+//     };
+//     if (currentPosition == slidesCount) {
+//         buttons[1].classList.add('inactive');
+//     }
+//     if (currentPosition > 0) {
+//         buttons[0].classList.remove('inactive');
+//     }
+// };
+// Akhir Card Slider2
+
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+		items.forEach((el) => {
+			const minPerSlide = 4
+			let next = el.nextElementSibling
+			for (var i=1; i<minPerSlide; i++) {
+				if (!next) {
+            // wrap carousel by using first child
+            next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
